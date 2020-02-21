180f689aec9d8aa355484f285eba6b68f564e2f3
jswelsh <james.sinclair.welsh@gmail.com>
2020-02-20 23:25:33 -0800

    instead of for loop, use object.keys(x).forEach, seems more concise, longer though

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-function most_used_word(str) {
+
+/* function most_used_word(str) {
     let wordCounts = { };
     let words = str.split(/\s/);
     for(let i = 0; i < words.length; i++) {
@@ -8,6 +9,24 @@ function most_used_word(str) {
         document.write(word + ": " + wordCounts[word] + ".")
     })
     console.log(wordCounts)
-}
+} */
+
+function most_used_word(str) {
+    let wordCounts = { };
+    let mostUsedWord = "";
+    let mostUsedWordCount = 0;
+    str.split(/\s/).forEach(word => {
+        wordCounts[word] = (wordCounts[word] || 0) + 1;
+    });
+    Object.keys(wordCounts).forEach(word => {
+        //document.write(word + ": " + wordCounts[word] + ".")
+        if(wordCounts[word] > mostUsedWordCount) {
+            mostUsedWord = word;
+            mostUsedWordCount = wordCounts[word]
+        }
+    })
+    document.write(mostUsedWord + " was used " + mostUsedWordCount + " times.")
+    console.log(wordCounts)
+} 
 
 most_used_word('alpha bravo alpha delta bravo alpha charlie charlie golf charlie foxtrot charlie')  
